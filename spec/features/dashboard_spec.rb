f28a890af5c5393cd69323394ee498dc3d2c772c
Teddy <theodore.widom@gmail.com>
2014-08-02 18:28:34 -0400

    Sign in user for feature specs
    Uses Clearance::Backdoor middleware

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe 'Dashboard' do
   it 'Is the root page' do
-    visit root_path
+    user = create(:user)
+    visit root_path(as: user)
     expect(page).to have_selector 'h1', text: 'Expenses dashboard'
   end
 end
