48aefd6f71272f043516750678249f1d1a6dccda
Teddy <theodore.widom@gmail.com>
2014-08-02 18:35:18 -0400

    Add validations on User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+User.create!(name: 'Teddy Widom', email: 'theodore.widom@gmail.com', password: 'password')
