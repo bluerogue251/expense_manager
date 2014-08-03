dfde997ea14057a8701e134cd1b0bfc1fa2638f8
Teddy <theodore.widom@gmail.com>
2014-08-03 00:00:07 -0400

    Tweak exchange rate validations
    Anchor and float currencies may not be the same
    Rate must be a number greater than 0

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,5 +1,16 @@
 class ExchangeRate < ActiveRecord::Base
   CURRENCIES = %w(USD CAD CNY HKD)
-  validates :rate, :starts_on, presence: true
+  validates :starts_on, presence: true
+  validates :rate, numericality: { greater_than: 0 }
   validates :anchor, :float, inclusion: { in: CURRENCIES, allow_blank: false }
+
+  validate :anchor_and_float_cannot_be_the_same
+
+  private
+
+  def anchor_and_float_cannot_be_the_same
+    if anchor == float
+      errors.add(:anchor, 'and float currencies cannot be the same')
+    end
+  end
 end
--- spec/models/exchange_rate_spec.rb
+++ spec/models/exchange_rate_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 describe ExchangeRate do
-  it { should validate_presence_of(:rate) }
+  it { should validate_numericality_of(:rate).is_greater_than(0) }
   it { should validate_presence_of(:starts_on) }
 
   it { should ensure_inclusion_of(:anchor).in_array(ExchangeRate::CURRENCIES).allow_blank(false) }
   it { should ensure_inclusion_of(:float).in_array(ExchangeRate::CURRENCIES).allow_blank(false) }
+
+  it "Cannot have the same anchor and float currencies" do
+    rate = ExchangeRate.new(anchor: "USD", float: "USD")
+    expect(rate).to_not be_valid
+    expect(rate.errors.full_messages).to include "Anchor and float currencies cannot be the same"
+  end
 end
