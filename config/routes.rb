80835deb7e10ef457cdbade3e32916fe3f2f88d8
Teddy <theodore.widom@gmail.com>
2014-08-06 23:46:02 -0400

    Add nav feature spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'dashboard#show'
   resources :expenses, except: [:new, :show]
+  resources :users, only: :edit
 end
---
+++ spec/features/navigation_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature "Navigation" do
+  scenario "Clicking the Dashboard link navigates to the dashboard (home page)" do
+    user = create(:user)
+    visit expenses_path(as: user)
+    click_link "Dashboard"
+    expect(current_path).to eq root_path
+  end
+
+  scenario "Clicking the Expenses link navigates to the expense index page" do
+    user = create(:user)
+    visit root_path(as: user)
+    click_link "Expenses"
+    expect(current_path).to eq expenses_path
+  end
+
+  scenario "Clicking the Profile link navigates to the profile page" do
+    user = create(:user)
+    visit root_path(as: user)
+    click_link "Profile"
+    expect(current_path).to eq edit_user_path(user)
+  end
+end
