eef5dcd94bd87ab9a4aae4d27263e3138004a66a
Teddy <theodore.widom@gmail.com>
2014-09-03 16:07:15 -0400

    Try action_controller.asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.default_url_options = { host: 'expensemanager.teddywidom.com' }
+  config.action_mailer.default_url_options = { host: 'http://expensemanager.teddywidom.com' }
+  config.action_controller.asset_host = 'expensemanager.teddywidom.com'
+
 end
